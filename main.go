31b4b1dbb1f37f71bb6ba1f9298199183998895e
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-07-25 00:18:50 +0530

    # commit : all article func

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import {
+import (
 	"fmt"
 	"log"
 	"net/http"
 	"encoding/json"
+)
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Desc", Content: "Test Content"},
+	}
+	fmt.Println("Endpoint Hit: All Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
 }
